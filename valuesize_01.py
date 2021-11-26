9bd91e374017d08429ce0972f1b03356af1f51f6
FIRST_NAME LAST_NAME <otter.oh@gmail.com>
2021-11-26 10:57:38 +0900

    get size value in memory

---
+++ valuesize_01.py
@@ -0,0 +1,18 @@
+second = 50
+
+def func01(first01):
+    first = 10
+    func02(first01)
+    return
+
+def func02(first02):
+    first = 20
+    return
+
+if __name__ == '__main__':
+    # first = 30
+    # func01(first)
+    a = 30
+    b = 4.2
+    c = 'G'
+    pass
