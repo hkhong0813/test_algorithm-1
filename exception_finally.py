9c515fa08336e4e09c691d693dd61abd07475640
FIRST_NAME LAST_NAME <otter.oh@gmail.com>
2021-11-30 14:53:01 +0900

    modify

--- exception_finally.py
+++ exception_finally.py
@@ -2,10 +2,9 @@ def func():
     pass
     return
 
-
 if __name__ == '__main__':
     try:
-      fh = open('./testfile.txt', 'w')  # '../testfile.txt'
+      fh = open('../testfile.txt', 'w')  # '../testfile.txt'
       fh.write('This is exception finally.')
     except Exception as e:
         pass
